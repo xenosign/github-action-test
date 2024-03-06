8d4759fc2b2596ab5c54a4bedb3cd5382cf4071f
xenosign <xenosign@naver.com>
2024-03-06 16:00:24 +0900

    dev 환경으로 변경

--- .lighthouse.js
+++ .lighthouse.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: { url: ["http://localhost:3000"], collect: { numberOfRuns: 5 } },
     upload: {
-      startServerCommand: "npm run start",
+      startServerCommand: "npm run dev",
       target: "temporary-public-storage",
     },
   },
