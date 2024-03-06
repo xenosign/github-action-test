0ccd25037452f00402c39198c9e7b6e2781aa990
xenosign <xenosign@naver.com>
2024-03-06 16:04:58 +0900

    빌드 폴더 포함

--- .lighthouse.js
+++ .lighthouse.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: { url: ["http://localhost:3000"], collect: { numberOfRuns: 5 } },
     upload: {
-      startServerCommand: "npm run dev",
+      startServerCommand: "npm run start",
       target: "temporary-public-storage",
     },
   },
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3moYVNw8qB/aJ6tNYuqwJoetN3mXU5VddnYC1j8l6pY=\"\n}"
